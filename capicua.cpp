cc48ef0b2c0d453a24931c7a37d9eff16a0142f6
Mario Pace <pacemario11@gmail.com>
2016-07-30 21:01:45 -0400

    Corregido

--- capicua.cpp
+++ capicua.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int main(){
 int reves, num, aux;
 
-printf("Ingrese el numero");
-scanf("%d", &num);
+puts("Ingrese numero");
+scanf("%d",&num);
 
 reves=0;
 aux=num;
 
 	while(aux>0){
-		reves*=10+aux%10;
-		aux*=/10;
+		reves=reves*10+aux%10;
+		aux=aux/10;
 	}
 
 	if(reves==num) printf("El numero es capicua");

aa11d5e04f2bbd3f52835d847d05c9b11b558ab2
Mario Pace <pacemario11@gmail.com>
2016-07-30 23:35:08 -0400

    Determina si un numero es primo

---
+++ nprimos.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int num, div;
+	
+	cout << "Ingrese un numero" << endl;
+	cin>>num;
+	
+	for(div=2; div<num/2; div++){
+		if(num%div==0)break;
+	}
+	if(div>=num/2) puts("Es primo");
+	else puts("No es primo");
+}
